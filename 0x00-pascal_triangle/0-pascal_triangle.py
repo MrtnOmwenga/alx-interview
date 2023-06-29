f0fe2a0b1bbd8168edd68efc34faddd6beaee412
MrtnOmwenga <martin36449@gmail.com>
2023-06-29 04:03:37 +0000

    Pascal's triangle in python

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,36 @@
+#!usr/bin/python3
+"""
+Create Pascal's triangle
+"""
+
+def pascal_triangle(n):
+    """
+    result- Full table of coefficients
+    n- Number of rows of pascal's table
+    i- Iterator for n
+    coefficients- row of table
+    prev- number directly above and to the left of the number
+    curr- number above and to the right of it
+    """
+    if n <= 0:
+        return []
+    result = []
+    for i in range(1, n + 1):
+        while (i <= n):
+            coefficients = [1]
+            if i == 1:
+                break
+            elif i == 2:
+                coefficients.append(1)
+                break
+            else:
+                while (len(coefficients)<len(result[-1])):
+                    prev = result[-1][len(coefficients)-1]
+                    curr = result[-1][len(coefficients)]
+                    coefficients.append(prev+curr)
+                else:
+                    coefficients.append(1)
+                    break
+                i += 1
+        result.append(coefficients)
+    return result
