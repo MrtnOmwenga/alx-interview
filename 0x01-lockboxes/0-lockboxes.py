38f966fa71e1c4431ad6bad4360213818283ab4e
MrtnOmwenga <martin36449@gmail.com>
2023-07-06 22:56:06 +0000

    Lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Lockboxes
+"""
+
+
+def canUnlockAll(boxes):
+
+    unlocked = [boxes[0]]
+
+    for box in unlocked:
+        for key in box:
+            if boxes[key] not in unlocked:
+                try:
+                    unlocked.append(boxes[key])
+                except Exception:
+                    break
+    
+    if len(unlocked) != len(boxes):
+        return False
+    return True
