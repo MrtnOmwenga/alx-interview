431792808711e21fa9193e4277cbf85ced8d5762
MrtnOmwenga <martin36449@gmail.com>
2023-07-20 22:54:16 +0000

    Debugging

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,88 +1,47 @@
 #!/usr/bin/python3
-'''A script for parsing HTTP request logs.
-'''
+"""
+Log parsing script
+"""
+import sys
 import re
 
 
-def extract_input(input_line):
-    '''Extracts sections of a line of an HTTP request log.
-    '''
-    fp = (
-        r'\s*(?P<ip>\S+)\s*',
-        r'\s*\[(?P<date>\d+\-\d+\-\d+ \d+:\d+:\d+\.\d+)\]',
-        r'\s*"(?P<request>[^"]*)"\s*',
-        r'\s*(?P<status_code>\S+)',
-        r'\s*(?P<file_size>\d+)'
-    )
-    info = {
-        'status_code': 0,
-        'file_size': 0,
-    }
-    log_fmt = '{}\\-{}{}{}{}\\s*'.format(fp[0], fp[1], fp[2], fp[3], fp[4])
-    resp_match = re.fullmatch(log_fmt, input_line)
-    if resp_match is not None:
-        status_code = resp_match.group('status_code')
-        file_size = int(resp_match.group('file_size'))
-        info['status_code'] = status_code
-        info['file_size'] = file_size
-    return info
+ip_r = r'(\d{1,3}\.){3}\d{1,3}'
+date_r = r'\d{4}-\d{2}-\d{2}\s\d{2}:\d{2}:\d{2}\.\d{6}'
+method_r = r'"GET /projects/260 HTTP/1\.1"'
+status_code_r = r' \d{3} '
+file_size_r = r'\d{1,4}\n'
+regex = r'{} - \[{}\] {}{}{}'.format(ip_r, date_r,
+                                     method_r, status_code_r,
+                                     file_size_r)
 
 
-def print_statistics(total_file_size, status_codes_stats):
-    '''Prints the accumulated statistics of the HTTP request log.
-    '''
-    print('File size: {:d}'.format(total_file_size), flush=True)
-    for status_code in sorted(status_codes_stats.keys()):
-        num = status_codes_stats.get(status_code, 0)
-        if num > 0:
-            print('{:s}: {:d}'.format(status_code, num), flush=True)
+def log(codes_list, total_size):
+    """ Logs output """
+    codes_list.sort()
+    print("File size: {}".format(total_size), flush=True)
+    for code in set(codes_list):
+        print("{}: {}".format(code, codes_list.count(code)), flush=True)
 
 
-def update_metrics(line, total_file_size, status_codes_stats):
-    '''Updates the metrics from a given HTTP request log.
-
-    Args:
-        line (str): The line of input from which to retrieve the metrics.
-
-    Returns:
-        int: The new total file size.
-    '''
-    line_info = extract_input(line)
-    status_code = line_info.get('status_code', '0')
-    if status_code in status_codes_stats.keys():
-        status_codes_stats[status_code] += 1
-    return total_file_size + line_info['file_size']
-
-
-def run():
-    '''Starts the log parser.
-    '''
-    line_num = 0
-    total_file_size = 0
-    status_codes_stats = {
-        '200': 0,
-        '301': 0,
-        '400': 0,
-        '401': 0,
-        '403': 0,
-        '404': 0,
-        '405': 0,
-        '500': 0,
-    }
+if __name__ == "__main__":
+    codes_list = []
+    total_size = 0
+    count = 0
     try:
-        while True:
-            line = input()
-            total_file_size = update_metrics(
-                line,
-                total_file_size,
-                status_codes_stats,
-            )
-            line_num += 1
-            if line_num % 10 == 0:
-                print_statistics(total_file_size, status_codes_stats)
+        for line in sys.stdin:
+            count += 1
+            searched_line = re.search(regex, line)
+            if searched_line:
+                try:
+                    status_code = int(re.search(status_code_r, line).group())
+                    file_size = int(re.search(file_size_r, line).group())
+                    codes_list.append(status_code)
+                    total_size += file_size
+                except Exception:
+                    pass
+                if count % 10 == 0:
+                    log(codes_list, total_size)
+        # log(codes_list, total_size)
     except (KeyboardInterrupt, EOFError):
-        print_statistics(total_file_size, status_codes_stats)
-
-
-if __name__ == '__main__':
-    run()
+        log(codes_list, total_size)
